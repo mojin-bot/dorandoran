25ee6e0fa91ba3ddd04b197fb44ab668104137d0
mojin <riverscot@gmail.com>
2025-10-02 09:19:04 +0900

    Create Icon.tsx

---
+++ app/components/Icon.tsx
@@ -0,0 +1,9 @@
+// app/components/Icon.tsx
+import { Icon as Iconify } from "@iconify/react";
+import icons from "@/app/data/icons.json";
+
+type Props = { name: keyof typeof icons; className?: string };
+
+export function Icon({ name, className }: Props) {
+  return <Iconify icon={icons[name]} className={className} />;
+}
